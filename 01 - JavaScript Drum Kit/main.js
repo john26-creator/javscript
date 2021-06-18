9cadf1294e1406eb68c35a9c39ae429e91a5cf47
Jonathan <jonathan.amselem.pro@gmail.com>
2021-06-18 12:21:46 +0200

    second commit correction

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,8 +1,8 @@
-const audioElements = document.querySelectorAll("kbd");
-const body = document.getElementsByTagName("body")[0];//ligne a garder, ca permet de selectionner tout le body
+const kbds = document.querySelectorAll("kbd");
+const body = document.body;//ligne a garder, ca permet de selectionner tout le body
 
 
-Array.prototype.forEach.call(audioElements, element =>
+Array.prototype.forEach.call(kbds, element =>
      element.addEventListener("click",
         function (event) {
             var letter = event.target.textContent;
@@ -13,4 +13,7 @@ Array.prototype.forEach.call(audioElements, element =>
         function (event) {
             var key = event.key.toUpperCase();
             document.getElementById(key).play();
+            var kbd = document.querySelector("." + key);
+            kbd.parentElement.classList.toggle("playing");
+            setTimeout (function () {kbd.parentElement.classList.toggle("playing");}, 2000);
         });
